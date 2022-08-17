f5050e06359dd5b0715f3bab14ef684d40e4b722
albertchen857 <104204390+albertchen857@users.noreply.github.com>
2022-08-17 20:41:32 +1000

    dodge-ball

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
-basic.clearScreen()
-basic.showString("Hide-ball")
-let location = 2
-let dd = 0
 let dd2 = 0
+let dd = 0
+basic.clearScreen()
+basic.showString("dodge-ball")
+let location = 2
 led.plot(2, 4)
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     if (input.buttonIsPressed(Button.A) && location > 0) {
         led.toggle(location, 4)
         location += -1
@@ -17,12 +16,10 @@ basic.forever(function on_forever() {
         led.plot(location, 4)
         basic.pause(100)
     }
-    
 })
-basic.forever(function on_forever2() {
-    
+basic.forever(function () {
     dd = randint(0, 4)
-    for (let index1 = 0; index1 < 4; index1++) {
+    for (let index1 = 0; index1 <= 3; index1++) {
         led.plot(dd, index1)
         basic.pause(1000)
         led.toggle(dd, index1)
@@ -32,15 +29,12 @@ basic.forever(function on_forever2() {
             basic.showIcon(IconNames.No)
         }
     }
-    
 })
-basic.forever(function on_forever3() {
-    
+basic.forever(function () {
     if (dd == dd2) {
         dd2 = randint(0, 4)
     }
-    
-    for (let index2 = 0; index2 < 4; index2++) {
+    for (let index2 = 0; index2 <= 3; index2++) {
         led.plot(dd2, index2)
         basic.pause(1000)
         led.toggle(dd2, index2)
@@ -50,5 +44,4 @@ basic.forever(function on_forever3() {
             basic.showIcon(IconNames.No)
         }
     }
-    
 })
